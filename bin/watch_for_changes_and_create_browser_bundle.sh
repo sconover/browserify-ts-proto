5d8877630ad19ea98a2502b26b0ac7bcf0acaa1a
Steve Conover <sconover@gmail.com>
2017-08-18 08:47:12 +0200

    extract the sourcemap from the bundle and stop committing it

--- bin/watch_for_changes_and_create_browser_bundle.sh
+++ bin/watch_for_changes_and_create_browser_bundle.sh
@@ -6,4 +6,7 @@ this_dir=$(dirname $0)
 # Use watchify (browserify) to watch for changes in any ts files
 # and then re-gen the js bundle.
 
-bash -c "cd $this_dir/../nodejs && node node_modules/watchify/bin/cmd.js ../ts/ui/Init.ts -v -d -p [ tsify --project ../ts ] -o ../web/app.js"
+# extract the soucemap to a separate file using exorcist.
+# (the sourcemap file is .gitignore'd)
+
+bash -c "cd $this_dir/../nodejs && node node_modules/watchify/bin/cmd.js ../ts/ui/Init.ts -v -d -p [ tsify --project ../ts ] -o 'node_modules/.bin/exorcist ../web/app.js.map > ../web/app.js'"
